9852c749c2b1a427efe0a75bfc303b072bf19176
shinov42 <shinov42@gmail.com>
2017-11-19 16:40:59 +0300

    bugfix
    added overriding for remove
    fixed NPE in remove
    changed "size" back to private
    added getSize()
    checked the working of both functions

--- src/BinaryTreeTest.java
+++ src/BinaryTreeTest.java
@@ -18,19 +18,22 @@ public class BinaryTreeTest {
         tree.add(5);
         tree.add(7);
         tree.remove(10);
-        assertEquals(2, tree.size);
+        tree.remove(5);
+        tree.remove(7);
+        tree.remove(1);
+        assertEquals(0, tree.getSize());
         tree.add(3);
         tree.add(1);
         tree.add(4);
-        tree.remove(1);
-        assertEquals(4, tree.size);
+        tree.remove(3);
+        assertEquals(2, tree.getSize());
         tree.add(8);
         tree.add(15);
         tree.add(20);
         tree.remove(15);
         tree.remove(20);
         tree.remove(8);
-        assertEquals(4, tree.size);
+        assertEquals(2, tree.getSize());
     }
 
     @org.junit.Test
@@ -47,21 +50,21 @@ public class BinaryTreeTest {
         tree.add(30);
         tree.add(20);
         tree.remove(30);
-        SortedSet abc= tree.headSet(5);
+        SortedSet abc = tree.headSet(5);
         TreeSet<Integer> setabc = new TreeSet<>();
         setabc.add(1);
         setabc.add(3);
         setabc.add(4);
-        assertEquals((SortedSet)setabc, abc);
+        assertEquals((SortedSet) setabc, abc);
         setabc.add(5);
         setabc.add(7);
         setabc.add(8);
         abc = tree.headSet(10);
-        assertEquals((SortedSet)setabc, abc);
+        assertEquals((SortedSet) setabc, abc);
         setabc.add(10);
         setabc.add(15);
         abc = tree.headSet(20);
-        assertEquals((SortedSet)setabc, abc);
+        assertEquals((SortedSet) setabc, abc);
     }
 
 
